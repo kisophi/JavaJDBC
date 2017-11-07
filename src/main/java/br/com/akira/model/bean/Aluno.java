43c15939c3ede995494154d319aa122387714c71
Akira Matsumoto <akiramatsumoto@live.com>
2017-11-07 18:52:15 -0200

    Add Classe Aluno

---
+++ src/main/java/br/com/akira/model/bean/Aluno.java
@@ -0,0 +1,35 @@
+package br.com.akira.model.bean;
+
+import java.util.Calendar;
+
+public class Aluno {
+
+	private int id;
+	private String nome;
+	private Calendar dataCadastro;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Calendar getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Calendar dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+}
